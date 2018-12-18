453f01c1298f1e7efed549f5fc0d927d66030b6f
Alexander Kern <kern.alex@berkeley.edu>
2018-12-18 12:39:48 -0800

    Insert implemented, used mapwhich may cost un-nessicary memory use

--- dictionary.go
+++ dictionary.go
@@ -1,6 +1,47 @@
 package trie
 
-
+/*
+maps each element to the
+tree
+*/
 type tNode struct {
+	letters map[string]*tNode
+	end bool
+}
 
+/*
+Basic trieTree structure
+*/
+type trieTree struct {
+	root *tNode
+}
+func NewTrieTree() trieTree{
+	var tree = trieTree{}
+	tree.root = &tNode{make(map[string]*tNode), false}
+	return tree
+}
+
+/*
+insert function
+*/
+func (t *trieTree) Insert(s string) {
+	temp := t.root
+	i := 0
+	for ; i < len(s) - 1; i++ {
+		_, ok := (*temp).letters[string(s[i])]
+		if ok {
+			temp = temp.letters[string(s[i])]
+		} else {
+			temp.letters[string(s[i])] = &tNode{make(map[string]*tNode), false}
+			temp = temp.letters[string(s[i])]
+		}
+	}
+	temp.end = true
+	_, ok := (*temp).letters[string(s[i])]
+	if ok {
+		temp = temp.letters[string(s[i])]
+	} else {
+		temp.letters[string(s[i])] = &tNode{make(map[string]*tNode), false}
+		temp = temp.letters[string(s[i])]
+	}
 }
